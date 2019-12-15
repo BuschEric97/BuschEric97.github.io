95fbcb1a50b56328d29ae20d6260ef32de89a53c
Eric Busch <buscheric97@gmail.com>
2019-12-15 00:50:00 -0600

    Modified tab 1

--- toggleDarkHome.js
+++ toggleDarkHome.js
@@ -5,7 +5,8 @@ function toggleDark() {
     document.querySelectorAll("h3").forEach(element => {element.style.color = "#000000";});
     document.querySelectorAll("p").forEach(element => {element.style.color = "#000000";});
     document.getElementById("leftside").style.background = "#e6e6e6";
-    document.getElementById("main").style.background = "#f2f2f2"
+    document.getElementById("main").style.background = "#f2f2f2";
+    document.getElementById("body").style.background = "#f2f2f2";
     isDark = "f";
   } else {
     document.querySelectorAll("h2").forEach(element => {element.style.color = "#ffffff";});
@@ -13,6 +14,7 @@ function toggleDark() {
     document.querySelectorAll("p").forEach(element => {element.style.color = "#ffffff";});
     document.getElementById("leftside").style.background = "#404040";
     document.getElementById("main").style.background = "#4d4d4d";
+    document.getElementById("body").style.background = "#4d4d4d";
     isDark = "t";
   }
 }
--- toggleDarkTab1.js
+++ toggleDarkTab1.js
@@ -4,15 +4,17 @@ function toggleDark() {
     document.querySelectorAll("h2").forEach(element => {element.style.color = "#000000";});
     document.querySelectorAll("h3").forEach(element => {element.style.color = "#000000";});
     document.querySelectorAll("p").forEach(element => {element.style.color = "#000000";});
-//    document.getElementById("leftside").style.background = "#e6e6e6";
-//    document.getElementById("main").style.background = "#f2f2f2"
+    document.getElementById("leftside").style.background = "#e6e6e6";
+    document.getElementById("main").style.background = "#f2f2f2";
+    document.getElementById("body").style.background = "#f2f2f2";
     isDark = "f";
   } else {
     document.querySelectorAll("h2").forEach(element => {element.style.color = "#ffffff";});
     document.querySelectorAll("h3").forEach(element => {element.style.color = "#ffffff";});
     document.querySelectorAll("p").forEach(element => {element.style.color = "#ffffff";});
-//    document.getElementById("leftside").style.background = "#404040";
-//    document.getElementById("main").style.background = "#4d4d4d";
+    document.getElementById("leftside").style.background = "#404040";
+    document.getElementById("main").style.background = "#4d4d4d";
+    document.getElementById("body").style.background = "#4d4d4d";
     isDark = "t";
   }
 }
